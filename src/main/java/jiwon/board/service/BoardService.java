89ec3d90fbda8735fbbc2530c8254ce2881b0e9b
JJiWoNing <jjiwoning12@gmail.com>
2022-09-04 20:03:18 +0900

    Board 기능 세팅

--- src/main/java/jiwon/board/service/BoardService.java
+++ src/main/java/jiwon/board/service/BoardService.java
@@ -3,8 +3,10 @@ package jiwon.board.service;
 import jiwon.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BoardService {
 

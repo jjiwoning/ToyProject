418f80328f3b5d65ae9635e3d7419e38d7507a5e
JJiWoNing <jjiwoning12@gmail.com>
2022-09-15 17:42:24 +0900

    코드 수정

--- src/main/java/jiwon/board/dto/BoardWriteDto.java
+++ src/main/java/jiwon/board/dto/BoardWriteDto.java
@@ -20,5 +20,4 @@ public class BoardWriteDto {
         return board;
     }
 
-
 }
--- src/main/java/jiwon/board/dto/MemberSaveDto.java
+++ src/main/java/jiwon/board/dto/MemberSaveDto.java
@@ -21,7 +21,6 @@ public class MemberSaveDto {
 
     @Email(message = "xxx@xxx.xxx의 이메일 형태로 작성해주세요.")
     @NotNull(message = "이메일은 필수 입니다.") // @Email 어노테이션이 null 값을 허용하기 때문에 넣어줘야함
-    // getter가 이상하게 작동해서 mail로 이름 변경
     private String mail;
 
     @NotEmpty(message = "전화번호는 필수입니다.")
